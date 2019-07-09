e5916bc07102c520e0f4a0cf57e675adba94af4c
Atte Torri <atte@torri.be>
2019-07-09 23:01:02 +0300

    started work on automated test

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include "bloom.h"
 
-int test() {
+int test(int test_size) {
   printf("Beginning test sequence\n");
-  printf("Testing to be implemented\n");
+  printf("Using %d hash functions\n", _HASH_NUM);
+  printf("Using %d bytes for Bloom filter\n", _FILTER_SIZE);
+  printf("Filling filter with %d elements\n", test_size);
+
+  return 0;
+
+  char buffer[100];
+  for(int i = 0; i < test_size; i++) {
+    itoa(i, buffer, 2);
+  }
+}
+
+void help() {
+  printf("Commands : add, query, debug, exit, help\n");
+  printf("Usage : add word, query word\n");
 }
 
 void interactive() {
@@ -33,13 +47,10 @@ void interactive() {
   }
 }
 
-void help() {
-  printf("Commands : add, query, debug, exit, help\nUsage : add word, query word\n");
-}
-
 int main(int argc, char *argv[]) {
+  int test_size = (argc > 2)?atoi(argv[2]):100;
   if(argc > 1) {
-    if(!strcmp(argv[1], "test") || !strcmp(argv[1], "-t")) test();
+    if(!strcmp(argv[1], "test") || !strcmp(argv[1], "-t")) test(test_size);
     if(!strcmp(argv[1], "interactive") || !strcmp(argv[1], "-i")) interactive();
   } else {
     printf("Please give argument 'interactive'/'-i' or 'test'/'-t'...\n");
